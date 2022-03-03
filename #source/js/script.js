57612f3d550f7e1ab19c0ebec805c5221d97348b
ZAKHAROV-artem <artemz092006@gmail.com>
2022-03-03 17:21:54 +0200

    first commit

---
+++ #source/js/script.js
@@ -0,0 +1,6 @@
+$('.com__slider').slick({
+    arrows:false,
+    dots:true,
+    autoplay:true,
+    autoplaySpeed:5000
+});
---
+++ js/script-dist.js
@@ -0,0 +1,1 @@
+$(".com__slider").slick({arrows:!1,dots:!0,autoplay:!0,autoplaySpeed:5e3});
